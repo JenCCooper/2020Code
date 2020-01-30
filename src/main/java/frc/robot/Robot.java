c1310e6211b237bea71a51ac72b2ff41ed472ee3
Muhammed Abdalla <muhabdalla718@gmail.com>
2020-01-30 17:38:31 -0500

    Yo!!!!

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,145 +1,122 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
+/* the project.
 /*----------------------------------------------------------------------------*/
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.networktables.NetworkTable;
-//import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.TimedRobot;
-//import edu.wpi.first.wpilibj.Timer;
-//import edu.wpi.first.wpilibj.command.Scheduler;
-//import frc.robot.commands.ArcadeDrive;
-import frc.robot.subsystems.DriveTrain;
-import frc.robot.subsystems.PID_Drive;
-/*import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import edu.wpi.first.wpilibj.SpeedControllerGroup;*/
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the TimedRobot
  * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
+ * creating this project, you must also update the build.gradle file in the
+ * project.
  */
 public class Robot extends TimedRobot {
-  public static DriveTrain driveTrain;
-  public static OI m_oi;
-  public static Joystick m_stick;
-  public RobotMap map;
-  //public ArcadeDrive m_ADrive;
-  //private final DifferentialDrive m_robotDrive = new DifferentialDrive(Left, Right);
-  //private final Timer m_timer = new Timer();
-  public PID_Drive piddrive = new PID_Drive(1, 1, 1);
-  NetworkTable table;
-
   /**
    * This function is run when the robot is first started up and should be used
    * for any initialization code.
    */
+  private WPI_VictorSPX FR = new WPI_VictorSPX(RobotMap.FR);
+  private WPI_VictorSPX FL = new WPI_VictorSPX(RobotMap.FL);
+  private CANSparkMax testMotor = new CANSparkMax(10, MotorType.kBrushless);;
+  //private WPI_TalonSRX TestController = new WPI_TalonSRX(0);
+  private WPI_TalonSRX BR = new WPI_TalonSRX(RobotMap.BR);
+  private WPI_TalonSRX BL = new WPI_TalonSRX(RobotMap.BL);
+
+  private Joystick joy = new Joystick(0);
+
+  private SpeedControllerGroup right;
+  private SpeedControllerGroup left;
+
+  private DifferentialDrive Drive;
+
   @Override
   public void robotInit() {
-    //m_oi = new OI();
-    driveTrain = new DriveTrain();
-    m_stick = new Joystick(RobotMap.m_stick);
-    //m_ADrive = new ArcadeDrive();
+    // Inverted settings
+    FR.setInverted(false);
+    BR.setInverted(false);
+    FL.setInverted(true);
+    BL.setInverted(true);
 
-    
-    //NetworkTableInstance inst = NetworkTableInstance.getDefault();
-    //table = inst.getTable("ChickenVision");
-    //table = inst.getTable("Joystick");
+    right = new SpeedControllerGroup(FR, BR);
+    left = new SpeedControllerGroup(BR, BL);
+
+    // Differential Driv Deadband percentage
+    Drive = new DifferentialDrive(left, right);
+    Drive.setDeadband(0.05);
+
+    // init encocder
+
+    /*
+    RightMaster.setSensorPhase(true);
+    RightMaster.setSensorPhase(false);
+
+    RightMaster.setSelectedSensorPosition(0, 0, 10);
+    LeftMaster.setSelectedSensorPosition(0, 0, 10);
+    */
   }
 
   @Override
   public void robotPeriodic() {
-   // super.robotPeriodic();
-  }
-
-  @Override
-  public void disabledInit() {
-
-  }
-
-  @Override
-  public void disabledPeriodic() {
-    //Scheduler.getInstance().run();
-  }
-
-  /**
-   * This function is run once each time the robot enters autonomous mode.
-   */
-  @Override
-  public void autonomousInit() {
-    //m_timer.reset();
-    //m_timer.start();
-
-  }
-
-  /**
-   * This function is called periodically during autonomous.
-   */
-  @Override
-  public void autonomousPeriodic() {
-    //Scheduler.getInstance().run(); 
-    // Drive for 2 seconds
-    
+    SmartDashboard.putNumber("Joy Stick X", joy.getRawAxis(0));
+    SmartDashboard.putNumber("Joy Stick Y", joy.getRawAxis(1));
+    SmartDashboard.putNumber("NEO Position", testMotor.getEncoder().getPosition());
+    SmartDashboard.putNumber("NEO Velocity", testMotor.getEncoder().getVelocity());
+    //SmartDashboard.putNumber("Motor Controller", TestController.get());
   }
   
-  /**
-   * This function is called once each time the robot enters teleoperated mode.
-   */
+  @Override
+  public void autonomousInit() {
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+  }
+
   @Override
   public void teleopInit() {
-    //m_timer.reset();
-    //m_timer.start();
-
   }
 
-  /**
-   * This function is called periodically during teleoperated mode.
-   * 
-   * return
-   */
-  //double x_value;
-
-  public double constrain(double num) {
-    if (num > 1) {
-      num = 1; 
-    }
-    else if (num < -1) {
-      num = -1;
-    }
-    return num;
-  }
-  public double getAngle(double x, double y){
-    return Math.atan(x/y)/Math.PI;
-  }
   @Override
   public void teleopPeriodic() {
-    //Scheduler.getInstance().run();
-    //piddrive.setSetpoint(getAngle(m_stick.getX(),m_stick.getY()));
-    DriveTrain.m_drive.arcadeDrive(m_stick.getY()-0.1,constrain(getAngle(m_stick.getX(),m_stick.getY())));
-    //piddrive.execute(m_stick.getY()-0.1);
-    /*
-    if (m_stick.getX() < 0.1 && m_stick.getX() > -0.1) {
-      x_value = 0;
-    } else {
-      x_value = m_stick.getX();
-    }
-    */
-    //m_ADrive.execute();
-    //m_robotDrive.arcadeDrive(m_stick.getY(), x_value);
-    //front_Left.set(ControlMode.PercentOutput, m_stick.getX());
-    //back_Left.set(ControlMode.PercentOutput, m_stick.getY()+0.1);
+    double speed = -contrain(joy.getRawAxis(1));
+    double turn = contrain(joy.getRawAxis(0));
+    
+    Drive.arcadeDrive(speed, turn);
+    testMotor.set(speed);
   }
-  /**
-   * This function is called periodically during test mode.
-   */
+
+  @Override
+  public void testInit() {
+  }
+
   @Override
   public void testPeriodic() {
+  }
+  private double contrain(double value){
+    if (value > 1){
+      return 1;
+    }
+    else if (value < -1){
+      return -1;
+    }
+    else{
+      return value;
+    }
   }
 }
