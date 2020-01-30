4018005c9b7f2e8cb498698bab3fb9c8f7ac8e2d
dwssmn <58951982+dwssmn@users.noreply.github.com>
2020-01-30 18:42:39 -0500

    ef

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -17,6 +17,22 @@ public class RobotMap {
     public static final int BR = 3;
     public static final int test = 10;
 
+<<<<<<< Updated upstream
     //private static final int DriveController = 0;
     //private static final int m_stick = 0;
 }
+=======
+	public static final int front_Left_ID = 2;
+	public static final int back_Left_ID = 4;
+	public static final int front_Right_ID = 1;//6
+    public static final int back_Right_ID = 3;
+	public static final int driverController = 0;
+	public static final int m_stick = 0;
+	public static final int topshooter_ID = 5;
+	public static final int bottomshooter_ID = 6;
+	public static final int deployClimb_ID = 7;
+	public static final int retractClimb_ID = 8;
+	public static final int gatherer_ID = 9;
+	public static final int indexer_ID = 10;
+}
+>>>>>>> Stashed changes
